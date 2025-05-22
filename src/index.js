e2ad5820fe9add21b4ddcf38320fbdf026ae44d9
ruxlsr <russelsonwa1@gmail.com>
2025-05-22 12:54:27 +0100

    suppresion des non necessaires

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const vehicleRoutes = require('./vehicle.routes');
-
-app.use(express.json());
-app.use('/vehicles', vehicleRoutes);
-
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
